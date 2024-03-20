0976970bd822a85532f15402337e2f503740d459
Agha Kazim Hussain <aghakazim10@gmail.com>
2024-03-21 02:00:53 +0500

    Update applicationRouter.js

--- backend/routes/applicationRouter.js
+++ backend/routes/applicationRouter.js
@@ -1,7 +1,15 @@
 import express from "express";
-
+import {
+  employerGetAllApplications,
+  jobseekerDeleteApplication,
+  jobseekerGetAllApplications,
+} from "../controllers/applicationController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+router.get("/employer/getall", isAuthenticated, employerGetAllApplications);
+router.get("/jobseeker/getall", isAuthenticated, jobseekerGetAllApplications);
+router.delete("/delete/:id", isAuthenticated, jobseekerDeleteApplication);
 
 export default router;
