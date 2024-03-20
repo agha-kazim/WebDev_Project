2d4377eb3a1f82c2cbae6288ea751f0f862313e6
Agha Kazim Hussain <aghakazim10@gmail.com>
2024-03-21 04:14:07 +0500

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -58,4 +58,10 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
       });
   });
   
-  
+export const getUser = catchAsyncErrors((req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
