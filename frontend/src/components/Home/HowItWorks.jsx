073a90289cdd0be1e278ced17fae17808340abcf
Agha Kazim Hussain <53783438+agha-kazim@users.noreply.github.com>
2024-05-22 00:50:47 +0500

    Update HowItWorks.jsx

--- frontend/src/components/Home/HowItWorks.jsx
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -8,30 +8,27 @@ const HowItWorks = () => {
     <>
       <div className="howitworks">
         <div className="container">
-          <h3>How JobZee Works</h3>
+          <h3>How JobPulse Works</h3>
           <div className="banner">
             <div className="card">
               <FaUserPlus />
               <p>Create Account</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
+              Get Started in Seconds. Creating an account is quick and easy. Simply enter your email and a password to start your journey.
               </p>
             </div>
             <div className="card">
               <MdFindInPage />
               <p>Find a Job/Post a Job</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
+              Discover Your Next Career Move. Explore thousands of job openings in your field. Find companies that align with your values and goals.  Start your job search today.
               </p>
             </div>
             <div className="card">
               <IoMdSend />
               <p>Apply For Job/Recruit Suitable Candidates</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Consequuntur, culpa.
+              Searching for top talent to join our innovative team. We offer a dynamic work environment, opportunities for growth, and competitive compensation.
               </p>
             </div>
           </div>
